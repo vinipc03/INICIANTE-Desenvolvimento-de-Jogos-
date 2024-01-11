b2532e6232c525aad4579f3b1c496c198eefc1e6
Vinícius Costa <vini.pc03@gmail.com>
2024-01-11 13:49:25 -0300

    adicionado animações de personagem  e colisores

---
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 vel;
+
+    public Transform floorCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0, 150));
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = vel;
+    }
+}
