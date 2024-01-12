1303aedbc65ae3c506221b126fddf49e9c0d9070
Vinícius Costa <vini.pc03@gmail.com>
2024-01-11 23:17:53 -0300

    Aulas 5 a 7

--- INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Scripts/PlayerController.cs
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     Vector2 vel;
 
     public Transform floorCollider;
+    public Transform skin;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +21,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && floorCollider.GetComponent<FloorCollider>().canJump == true)
         {
+            skin.GetComponent<Animator>().Play("Player_jump", -1);
+            rb.velocity = Vector2.zero;
+            floorCollider.GetComponent<FloorCollider>().canJump = false;
+            //AO TERMINAR O JOGO VOLTAR PARA AULA 6 APLICAR CORREÇÃO NO JUMP
             rb.AddForce(new Vector2(0, 150));
         }
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
 
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            skin.GetComponent<Animator>().SetBool("PlayerRun", true);
+        }
+        else
+        {
+            skin.GetComponent<Animator>().SetBool("PlayerRun", false);
+        }
     }
 
     private void FixedUpdate()
