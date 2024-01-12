c4473686fc69b2818eb27eb523c694a3c07843bc
Vinícius Costa <vini.pc03@gmail.com>
2024-01-12 18:04:43 -0300

    Aula 11

---
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Scripts/AttackCollider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCollider : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            if(player.GetComponent<PlayerController>().comboNum == 1)
+            {
+                collision.GetComponent<Character>().life--;
+            }
+
+            if (player.GetComponent<PlayerController>().comboNum == 2)
+            {
+                collision.GetComponent<Character>().life -= 2;
+            }
+
+        }
+    }
+}
