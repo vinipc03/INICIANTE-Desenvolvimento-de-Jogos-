ec3c7d5a713bb8bd613a89263ad8f6ba9ac83273
Vinícius Costa <vini.pc03@gmail.com>
2024-01-15 13:48:03 -0300

    Aulas 12 e 13, inimigos Keeper e Ghost

---
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Ghost/Script/GhostController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+
+    public Transform a;
+    public Transform b;
+
+    public Transform skin;
+
+    public bool goRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (goRight == true)
+        {
+            skin.localScale = new Vector3(-1, 1, 1);
+
+            if (Vector2.Distance(transform.position, b.position) < 0.1f)
+            {
+                transform.position = a.position;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, b.position, 1f * Time.deltaTime);
+        }
+        else
+        {
+            skin.localScale = new Vector3(1, 1, 1);
+            if (Vector2.Distance(transform.position, a.position) < 0.1f)
+            {
+                transform.position = b.position;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, a.position, 1f * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Character>().life--;
+        }
+    }
+}
