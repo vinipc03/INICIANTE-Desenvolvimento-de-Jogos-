88b5f2555d8733ea8a9269466e104fbe7e8bf736
Vinícius Costa <vini.pc03@gmail.com>
2024-01-16 22:20:00 -0300

    Armadilhas adicionadas

---
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Trap/BearTrap/BearTrap.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour
+{
+
+    Transform player;
+    public Transform skin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            skin.GetComponent<Animator>().Play("Stuck", -1);
+
+            collision.transform.position = transform.position;
+            collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            
+            collision.GetComponent<PlayerController>().skin.GetComponent<Animator>().SetBool("PlayerRun", false);
+            collision.GetComponent<PlayerController>().skin.GetComponent<Animator>().Play("Player_Idle", -1);
+            collision.transform.GetComponent<Character>().life--;
+
+            GetComponent<BoxCollider2D>().enabled = false;
+            
+            player = collision.transform;
+
+            collision.GetComponent<PlayerController>().enabled = false;
+
+            Invoke("ReleasePlayer", 2);
+        }
+    }
+
+    void ReleasePlayer()
+    {
+        player.GetComponent<PlayerController>().enabled = true;
+    }
+}
---
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Trap/SpikeTrap1/Script/SpikeTrap1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            collision.transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 180));
+            collision.transform.GetComponent<Character>().life--;
+        }
+    }
+}
