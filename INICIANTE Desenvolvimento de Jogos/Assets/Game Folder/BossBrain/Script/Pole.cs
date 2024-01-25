9e4cfa660ea836637c42cf445c08c0b97f51f3ce
Vinícius Costa <vini.pc03@gmail.com>
2024-01-25 13:55:42 -0300

    Mecânicas sala do boss

---
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/BossBrain/Script/Pole.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pole : MonoBehaviour
+{
+    public Transform spike;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Equals("AttackCollider"))
+        {
+            spike.GetComponent<Animator>().Play("Spike", -1);
+            GetComponent<Animator>().Play("Pole", -1);
+        }
+    }
+}
---
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/BossBrain/Script/Spike.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    Transform boss;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            boss = collision.transform;
+            collision.transform.parent = transform;
+            collision.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    public void ReleaseBoss()
+    {
+        boss.parent = null;
+    }
+}
