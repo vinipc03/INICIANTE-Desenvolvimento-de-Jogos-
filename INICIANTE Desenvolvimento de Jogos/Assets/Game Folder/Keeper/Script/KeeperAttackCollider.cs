5ba28c2f279503e1f6ce2a15324cfd3d44513167
Vinícius Costa <vini.pc03@gmail.com>
2024-02-04 23:10:15 -0300

    some fixes

--- INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Keeper/Script/KeeperAttackCollider.cs
+++ INICIANTE Desenvolvimento de Jogos/Assets/Game Folder/Keeper/Script/KeeperAttackCollider.cs
@@ -6,17 +6,7 @@ public class KeeperAttackCollider : MonoBehaviour
 {
 
     public Transform player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
